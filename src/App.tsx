98571f599983128e7b99a796bfe5d1af9e6cf087
Athos Franco <athos.francof@gmail.com>
2023-01-17 02:13:35 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const currentDependencies = {
-    "@emotion/react": "^11.10.5",
-    "@emotion/styled": "^11.10.5",
-    "@mui/material": "^5.11.4",
-    "@types/react-router-dom": "^5.3.3",
-    dayjs: "^1.11.7",
-    react: "^18.2.0",
-    "react-dom": "^18.2.0",
-    "react-router-dom": "^6.6.2",
-    zustand: "^4.3.2",
-  };
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <pre>{JSON.stringify(currentDependencies, null, 4)}</pre>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Typography>Primeiro commit</Typography>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
